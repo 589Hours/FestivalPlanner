2cb1d6a5352a0492dfe9e5506ead2c30ce3a9dea
589Hours <rik.vanham@ziggo.nl>
2024-02-16 15:57:08 +0100

    Gewerkt aan TableView en code verbeterd
    Samen met Kwint de functionaliteit aan tableview toegevoegd dat de cellen worden ingevuld volgens de categorie naam. Ook hebben we de constructor van performance verbeterd. Tenslotte ook nog een extra performance aan de test data toegevoegd.

--- src/Application/FestivalTableview.java
+++ src/Application/FestivalTableview.java
@@ -1,39 +1,82 @@
 package Application;
 
 
+import data.CellDataForTableView;
 import data.FestivalPlan;
+import data.Performance;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+import java.util.ArrayList;
 
 
 public class FestivalTableview extends TableView {
 
     private FestivalPlan festivalPlan;
+    private TableColumn beginTimeColumn;
+    private TableColumn endTimeColumn;
+    private TableColumn stageColumn;
+    private TableColumn artistColumn;
+    private TableColumn popularityColumn;
+    private TableColumn genreColumn;
 
-    public FestivalTableview(){
+
+    public FestivalTableview(FestivalPlan festivalPlan){
         //TODO receive data for the table
+        this.festivalPlan = festivalPlan;
         setUpTable();
     }
     public void setUpTable(){
-        TableColumn beginTimeColumn = new TableColumn("Begin time");
-        TableColumn endTimeColumn = new TableColumn("End time");
-        TableColumn stageColumn = new TableColumn("Stage");
-        TableColumn artistColumn = new TableColumn("Artist");
-        TableColumn popularityColumn = new TableColumn("Popularity");
-        TableColumn genre = new TableColumn("Genre");
-
+        beginTimeColumn = new TableColumn("Begin time");
+        endTimeColumn = new TableColumn("End time");
+        stageColumn = new TableColumn("Stage");
+        artistColumn = new TableColumn("Artist");
+        popularityColumn = new TableColumn("Popularity");
+        genreColumn = new TableColumn("Genre");
 
         /*Hiermee set je de sizes naar 1/6 van de totale lengte.
         Hierdoor worden alle stukjes even groot */
+        setColumnsWidth();
+
+        //add columns to the table
+        getColumns().addAll(beginTimeColumn,endTimeColumn,stageColumn,artistColumn,popularityColumn,genreColumn);
+
+        //set the values for the cells according to the CellData... object
+        setCellValue(beginTimeColumn, "beginTime");
+        setCellValue(endTimeColumn, "endTime");
+        setCellValue(stageColumn, "stage");
+        setCellValue(artistColumn, "artistName");
+        setCellValue(popularityColumn, "popularity");
+        setCellValue(genreColumn, "genre");
+
+        /* per performance naar stringfactory object en tenslotte toevoegen
+        aan Arraylist die uiteindelijk observable word */
+
+        ArrayList<CellDataForTableView> data = new ArrayList<>();
+        for (Performance performance : festivalPlan.getPerformances()) {
+            CellDataForTableView cellDataForTableView = new CellDataForTableView(performance);
+            data.add(cellDataForTableView);
+        }
+
+        ObservableList<CellDataForTableView> dataToShow = FXCollections.observableArrayList(data);
+
+        setItems(dataToShow);
+    }
+
+    public void setColumnsWidth() {
         beginTimeColumn.prefWidthProperty().bind(this.widthProperty().divide(6));
         endTimeColumn.prefWidthProperty().bind(this.widthProperty().divide(6));
         stageColumn.prefWidthProperty().bind(this.widthProperty().divide(6));
         artistColumn.prefWidthProperty().bind(this.widthProperty().divide(6));
         popularityColumn.prefWidthProperty().bind(this.widthProperty().divide(6));
-        genre.prefWidthProperty().bind(this.widthProperty().divide(6));
+        genreColumn.prefWidthProperty().bind(this.widthProperty().divide(6));
+    }
 
-
-        getColumns().addAll(beginTimeColumn,endTimeColumn,stageColumn,artistColumn,popularityColumn,genre);
+    public void setCellValue(TableColumn tableColumn, String parameter){
+        tableColumn.setCellValueFactory(new PropertyValueFactory<CellDataForTableView, String>(parameter));
     }
 
 }
