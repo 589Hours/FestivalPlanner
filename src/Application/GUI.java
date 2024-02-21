f058693e995efa230d888fff0b96fcc7e99445ab
Falcon.G2 <ctw.vanderpluijm@student.avans.nl>
2024-02-21 14:50:58 +0100

    Knop is gaande, alleen de verdeling van alle elementen moeten we nog wel even brainstormen

--- src/Application/GUI.java
+++ src/Application/GUI.java
@@ -2,13 +2,26 @@ package Application;
 
 import data.FestivalPlan;
 import javafx.application.Application;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.*;
+import javafx.stage.Popup;
 import javafx.stage.Stage;
+import javafx.stage.Window;
 import org.jfree.fx.ResizableCanvas;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class GUI extends Application {
     private FestivalPlan festivalPlan;
@@ -17,19 +30,22 @@ public class GUI extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         BorderPane borderPane = new BorderPane();
+        BorderPane buttonPlacement = new BorderPane();
         canvas = new ResizableCanvas(g -> festivalBlockview.draw(g), borderPane);
 
         festivalPlan = new FestivalPlan();
 
         MenuBar menuBar = new MenuBar();
+
         Menu menu1 = new Menu("Saturday");
         MenuItem menuItem1 = new MenuItem("Tableview");
         MenuItem menuItem2 = new MenuItem("Blockview");
-        menu1.getItems().addAll(menuItem1, menuItem2);
-        Menu menu2 = new Menu("Sunday");
 
+        Menu menu2 = new Menu("Sunday");
         MenuItem menuItem3 = new MenuItem("Tableview");
         MenuItem menuItem4 = new MenuItem("Blockview");
+
+        menu1.getItems().addAll(menuItem1, menuItem2);
         menu2.getItems().addAll(menuItem3, menuItem4);
         menuBar.getMenus().addAll(menu1, menu2);
 
@@ -50,10 +66,46 @@ public class GUI extends Application {
 
         borderPane.setTop(menuBar);
 
+        Button artistButton = new Button("Add Artist");
+        buttonPlacement.setRight(artistButton);
+        borderPane.setBottom(buttonPlacement);
+
+        artistButton.setOnAction(event -> {
+            Stage artistStage = new Stage();
+            ComboBox stages = new ComboBox();
+            ComboBox timeStamps = new ComboBox();
+            ArrayList<String> times = new ArrayList<>();
+            Collections.addAll(times,"12:00","12:30","13:00","13:30","14:00","14:30","15:00","15:30","16:00","16:30","17:00","17:30","18:00","18:30","19:00","19:30","20:00","20:30","21:00","21:30","22:00","22:30","23:00","23:30","00:00","00:30","01:00","01:30","02:00");
+
+            stages.getItems().addAll(new Label("Main stage"));
+            for (String time : times){
+                timeStamps.getItems().add(new Label(time));
+            }
+
+            VBox vBox = new VBox(10);
+
+
+            vBox.getChildren().add(new Label("Artist Name"));
+            vBox.getChildren().add(new TextField());
+            vBox.getChildren().add(new Label("Stage where this artist will perform"));
+            vBox.getChildren().add(stages);
+            vBox.getChildren().add(new Label("Choose timeslot for the performance"));
+            vBox.getChildren().add(timeStamps);
+
+            Scene artistScene = new Scene(vBox);
+
+            artistStage.setHeight(500);
+            artistStage.setWidth(750);
+            artistStage.setTitle("Artist Information");
+            artistStage.setScene(artistScene);
+            artistStage.show();
+        });
+
         borderPane.setPrefSize(1700,800);
         Scene scene = new Scene(borderPane);
         primaryStage.setScene(scene);
         primaryStage.setTitle("Festival Planner");
         primaryStage.show();
     }
+
 }
