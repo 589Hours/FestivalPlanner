f69bbde12e1a0783d4dddca43c5931ab8adf9ab0
kwint <kwintjanleenders@gmail.com>
2024-02-22 12:21:14 +0100

    Save and load via file functionality added

--- src/data/FestivalPlan.java
+++ src/data/FestivalPlan.java
@@ -1,8 +1,9 @@
 package data;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 
-public class FestivalPlan {
+public class FestivalPlan implements Serializable {
     private ArrayList<Performance> performances;
     private ArrayList<Artist> artists;
     private ArrayList<Stage> stages;
