afd89adde4d1ba3794876ca243a4f2c28abe1b5b
Mohammed <m.lahlal@student.avans.nl>
2024-03-14 00:50:24 +0100

    Comments toegevoegd aan DeleteArtist , DeletePerformance , DeleteStage.

--- src/Application/Delete/DeleteArtist.java
+++ src/Application/Delete/DeleteArtist.java
@@ -16,56 +16,56 @@ import java.util.Optional;
 
 public class DeleteArtist {
     public DeleteArtist(FestivalPlan festivalPlan, FestivalBlockview festivalBlockview) {
-        Stage stage = new Stage();
-        ChoiceBox<data.Artist> artists = new ChoiceBox<>();
-        artists.getItems().addAll(festivalPlan.getArtists());
+        Stage stage = new Stage(); // Maakt een nieuwe stage
+        ChoiceBox<data.Artist> artists = new ChoiceBox<>(); // Maakt een choicebox
+        artists.getItems().addAll(festivalPlan.getArtists()); // Vult de choicebox met alle artiesten uit de festivalplan
 
-        Button deleteArtist = new Button("Delete");
-        Button cancelButton = new Button("Cancel");
+        Button deleteArtist = new Button("Delete"); // Maakt een delete button
+        Button cancelButton = new Button("Cancel"); // Maakt een cancel button
 
-        HBox buttons = new HBox();
-        buttons.getChildren().addAll(deleteArtist, cancelButton);
+        HBox buttons = new HBox(); // Maakt een hbox
+        buttons.getChildren().addAll(deleteArtist, cancelButton); // Vult de hbox met de buttons
 
-        VBox vBox = new VBox();
-        vBox.getChildren().addAll(artists, buttons);
-        vBox.setSpacing(10);
+        VBox vBox = new VBox(); // Maakt een vbox
+        vBox.getChildren().addAll(artists, buttons); // Vult de vbox met de choicebox en de buttons
+        vBox.setSpacing(10); // Zet de spatie tussen de elementen
 
-        Scene scene = new Scene(vBox);
-        stage.setScene(scene);
-        stage.setHeight(500);
-        stage.setWidth(750);
-        stage.show();
+        Scene scene = new Scene(vBox); // Maakt de scene aan
+        stage.setScene(scene); // Zet de scene van de stage
+        stage.setHeight(500); // Zet de hoogte van de stage
+        stage.setWidth(750); // Zet de breedte van de stage
+        stage.show(); // Toon de stage
 
         //buttons
-        cancelButton.setOnAction(event -> {
-            stage.close();
+        cancelButton.setOnAction(event -> { // Als de cancel button wordt aangeklikt
+            stage.close(); // Sluit de stage af
         });
 
-        deleteArtist.setOnAction(event -> {
-            if (artists.getItems().isEmpty()) {
-                Alert error = new Alert(Alert.AlertType.ERROR);
-                error.getDialogPane().setContent(new Label("Please select an artist to delete!"));
-                error.show();
-            } else {
-                Alert warning = new Alert(Alert.AlertType.WARNING);
-                ButtonType cancel = new ButtonType("Cancel", ButtonBar.ButtonData.CANCEL_CLOSE);
-                warning.getButtonTypes().addAll(cancel);
+        deleteArtist.setOnAction(event -> { // Als de delete button wordt aangeklikt
+            if (artists.getItems().isEmpty()) { // Als er geen artiesten zijn geselecteerd
+                Alert error = new Alert(Alert.AlertType.ERROR); // Maak een error
+                error.getDialogPane().setContent(new Label("Please select an artist to delete!")); // Zet de error tekst
+                error.show(); // Toon de error
+            } else { // Als er artiesten zijn geselecteerd
+                Alert warning = new Alert(Alert.AlertType.WARNING); // Maak een warning
+                ButtonType cancel = new ButtonType("Cancel", ButtonBar.ButtonData.CANCEL_CLOSE); // Maak een cancel button
+                warning.getButtonTypes().addAll(cancel); // Voeg de cancel button toe aan de warning
 
-                String artistName =  artists.getSelectionModel().getSelectedItem().getName();
-                warning.setHeaderText("Are you sure you want to delete " + artistName);
-                warning.setContentText("Deleting this " +artistName+ " will also delete Performances with them.");
+                String artistName =  artists.getSelectionModel().getSelectedItem().getName(); // Zet de artistnaam in een variabele
+                warning.setHeaderText("Are you sure you want to delete " + artistName); // Zet de headertekst van de warning
+                warning.setContentText("Deleting this " +artistName+ " will also delete Performances with them."); // Zet de tekst van de warning
 
-                Optional<ButtonType> result = warning.showAndWait();
+                Optional<ButtonType> result = warning.showAndWait(); // Toon de warning en wacht op een antwoord
 
-                if (result.get() == cancel) {
-                    warning.close();
-                } else {
-                    Artist artist = artists.getSelectionModel().getSelectedItem();
-                    festivalPlan.deleteArtist(artist);
-                    System.out.println("deleted");
-                    deletePerformanceWithArtist(festivalPlan, artist, festivalBlockview);
-                    warning.close();
-                    stage.close();
+                if (result.get() == cancel) { // Als de cancel button wordt aangeklikt
+                    warning.close(); // Sluit de warning af
+                } else { // Als de delete button wordt aangeklikt
+                    Artist artist = artists.getSelectionModel().getSelectedItem(); // Zet de geselecteerde artist in een variabele
+                    festivalPlan.deleteArtist(artist); // Verwijder de artist uit de festivalplan
+                    System.out.println("deleted"); // Print deleted
+                    deletePerformanceWithArtist(festivalPlan, artist, festivalBlockview); // Verwijder alle performances met de geselecteerde artist
+                    warning.close(); // Sluit de warning af
+                    stage.close(); // Sluit de stage af
                 }
             }
         });
--- src/Application/Delete/DeleteStage.java
+++ src/Application/Delete/DeleteStage.java
@@ -16,66 +16,66 @@ import java.util.Optional;
 
 public class DeleteStage {
     public DeleteStage(FestivalPlan festivalPlan, FestivalBlockview festivalBlockview) {
-        Stage stage = new Stage();
-        ChoiceBox<data.Stage> stages = new ChoiceBox<>();
-        stages.getItems().addAll(festivalPlan.getStages());
+        Stage stage = new Stage(); // Maakt een nieuwe stage
+        ChoiceBox<data.Stage> stages = new ChoiceBox<>(); // Maakt een nieuwe Choicebox
+        stages.getItems().addAll(festivalPlan.getStages()); // Vul de choicebox met alle stages uit festivalplan
 
-        Button deleteStage = new Button("Delete");
-        Button cancelButton = new Button("Cancel");
+        Button deleteStage = new Button("Delete"); // Maakt een delete button
+        Button cancelButton = new Button("Cancel"); // Maakt een cancel button
 
-        HBox buttons = new HBox();
-        buttons.getChildren().addAll(deleteStage, cancelButton);
+        HBox buttons = new HBox(); // Maakt een hbox
+        buttons.getChildren().addAll(deleteStage, cancelButton); // Vul de hbox met de buttons
 
-        VBox vBox = new VBox();
-        vBox.getChildren().addAll(stages, buttons);
-        vBox.setSpacing(10);
+        VBox vBox = new VBox(); // Maakt een vbox
+        vBox.getChildren().addAll(stages, buttons); // Vul de vbox met de choicebox en de buttons
+        vBox.setSpacing(10); // Zet de spatie tussen de elementen
 
-        Scene scene = new Scene(vBox);
-        stage.setScene(scene);
-        stage.setHeight(500);
-        stage.setWidth(750);
-        stage.show();
+        Scene scene = new Scene(vBox); // Maakt de scene aan
+        stage.setScene(scene); // Zet de scene van die stage
+        stage.setHeight(500); // Zet de hoogte van de stage
+        stage.setWidth(750); // Zet de breedte van de stage
+        stage.show(); // Toon de stage
 
         //buttons
-        cancelButton.setOnAction(event -> {
-            stage.close();
+        cancelButton.setOnAction(event -> { // Als de cancel button wordt aangeklikt
+            stage.close(); // Sluit de stages af
         });
 
-        deleteStage.setOnAction(event -> {
-            if(stages.getItems().isEmpty()) {
-                Alert error = new Alert(Alert.AlertType.ERROR);
-                error.getDialogPane().setContent(new Label("Please select an stage to delete!"));
-                error.show();
-            } else {
-                Alert warning = new Alert(Alert.AlertType.WARNING);
-                ButtonType cancel = new ButtonType("Cancel", ButtonBar.ButtonData.CANCEL_CLOSE);
-                warning.getButtonTypes().addAll(cancel);
+        deleteStage.setOnAction(event -> { // Als de delete button wordt aangeklikt
+            if(stages.getItems().isEmpty()) { // Als er geen stages zijn geselecteerd
+                Alert error = new Alert(Alert.AlertType.ERROR); // Maak een error
+                error.getDialogPane().setContent(new Label("Please select an stage to delete!")); // Zet de error tekst
+                error.show(); // Toon de error
+            } else { // Als er stages zijn geselecteerd
+                Alert warning = new Alert(Alert.AlertType.WARNING); // Maak een warning
+                ButtonType cancel = new ButtonType("Cancel", ButtonBar.ButtonData.CANCEL_CLOSE); // Maak een cancel button
+                warning.getButtonTypes().addAll(cancel); // Vul de warning met de cancel button
 
-                String stageName = stages.getSelectionModel().getSelectedItem().getName();
-                warning.setHeaderText("Are you sure you want to delete " + stageName);
-                warning.setContentText("Deleting this stage will also delete every performance on it!");
+                String stageName = stages.getSelectionModel().getSelectedItem().getName(); // Zet de stage naam
+                warning.setHeaderText("Are you sure you want to delete " + stageName); // Zet de warning header tekst
+                warning.setContentText("Deleting this stage will also delete every performance on it!"); // Zet de warning content tekst
 
-                Optional<ButtonType> result = warning.showAndWait();
+                Optional<ButtonType> result = warning.showAndWait(); // Toon de warning en wacht op de gebruiker
 
-                if(result.get() == cancel) {
-                    warning.close();
-                } else {
-                    data.Stage festivalStage = stages.getSelectionModel().getSelectedItem();
-                    festivalPlan.deleteStage(festivalStage);
-                    deletePerformancesOnStage(festivalPlan, festivalStage, festivalBlockview);
-                    warning.close();
-                    stage.close();
+                if(result.get() == cancel) { // Als de cancel button wordt aangeklikt
+                    warning.close(); // Sluit de warning af
+                } else { // Als de gebruiker de delete button aanklikt
+                    data.Stage festivalStage = stages.getSelectionModel().getSelectedItem(); // Zet de stage
+                    festivalPlan.deleteStage(festivalStage); // Verwijder de stage uit festivalplan
+                    deletePerformancesOnStage(festivalPlan, festivalStage, festivalBlockview); // Verwijder alle performances op de stage
+                    warning.close(); // Sluit de warning af
+                    stage.close(); // Sluit de stage af
                 }
             }
         });
     }
 
-    private void deletePerformancesOnStage(FestivalPlan festivalPlan, data.Stage festivalStage, FestivalBlockview festivalBlockview) {
-        ArrayList<Performance> temporaryPerformances = new ArrayList<>(festivalPlan.getPerformances());
-        for (Performance performance : temporaryPerformances) {
-            if (performance.getStage().equals(festivalStage)){
-                festivalPlan.deletePerformance(performance);
-                festivalBlockview.deleteBlock(performance);
+    private void deletePerformancesOnStage(FestivalPlan festivalPlan, data.Stage festivalStage, FestivalBlockview festivalBlockview) { // Verwijder alle performances op een stage
+        ArrayList<Performance> temporaryPerformances = new ArrayList<>(festivalPlan.getPerformances()); // Maak een tijdelijke arraylist met alle performances
+        for (Performance performance : temporaryPerformances) { // Loop door alle performances
+            if (performance.getStage().equals(festivalStage)){ // Als de performance op de stage gelijk is aan de stage die we willen verwijderen
+                festivalPlan.deletePerformance(performance); // Verwijder de performance uit festivalplan
+                festivalBlockview.deleteBlock(performance); // Verwijder de performance uit de blockview
             }
         }
     }
