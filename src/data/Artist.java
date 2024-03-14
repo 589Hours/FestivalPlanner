13a371645312e55feb258dcaf49b0c674e258399
Mohammed <m.lahlal@student.avans.nl>
2024-03-14 06:13:30 +0100

    Comments toegevoegd aan data en edit

--- src/data/Artist.java
+++ src/data/Artist.java
@@ -2,55 +2,72 @@ package data;
 
 import java.io.Serializable;
 
+// Klasse om een artiest te vertegenwoordigen, implementeert Serializable zodat objecten kunnen worden omgezet in een stroom van bytes voor serialisatie
 public class Artist implements Serializable {
-    private String name;
-    private int popularity;
-    private String genre;
-    private String artistInfo;
+    private String name; // Naam van de artiest
+    private int popularity; // Populariteit van de artiest
+    private String genre; // Genre van de artiest
+    private String artistInfo; // Informatie over de artiest
 
+    // Constructor voor het maken van een artiest met opgegeven naam, populariteit en genre
     public Artist(String name, int popularity, String genre) {
-        this.name = name;
-        this.popularity = popularity;
-        this.genre = genre;
-        this.artistInfo = "";
+        this.name = name; // Wijs de opgegeven naam toe aan de artiest
+        this.popularity = popularity; // Wijs de opgegeven populariteit toe aan de artiest
+        this.genre = genre; // Wijs het opgegeven genre toe aan de artiest
+        this.artistInfo = ""; // Initialiseer de informatie over de artiest als leeg
     }
+
+    // Constructor voor het maken van een artiest met opgegeven naam, populariteit, genre en artiestinformatie
     public Artist(String name, int popularity, String genre, String artistInfo){
-        this.name = name;
-        this.popularity = popularity;
-        this.genre = genre;
-        this.artistInfo = artistInfo;
+        this.name = name; // Wijs de opgegeven naam toe aan de artiest
+        this.popularity = popularity; // Wijs de opgegeven populariteit toe aan de artiest
+        this.genre = genre; // Wijs het opgegeven genre toe aan de artiest
+        this.artistInfo = artistInfo; // Wijs de opgegeven artiestinformatie toe aan de artiest
     }
 
+    // Getter voor de naam van de artiest
     public String getName() {
-        return name;
+        return name; // Geeft de naam van de artiest terug
     }
+
+    // Setter voor de naam van de artiest
     public void setName(String name) {
-        this.name = name;
+        this.name = name; // Wijs de opgegeven naam toe aan de artiest
     }
 
+    // Getter voor de populariteit van de artiest
     public int getPopularity() {
-        return popularity;
+        return popularity; // Geeft de populariteit van de artiest terug
     }
+
+    // Setter voor de populariteit van de artiest
     public void setPopularity(int popularity) {
-        this.popularity = popularity;
+        this.popularity = popularity; // Wijs de opgegeven populariteit toe aan de artiest
     }
 
+    // Getter voor het genre van de artiest
     public String getGenre() {
-        return genre;
+        return genre; // Geeft het genre van de artiest terug
     }
+
+    // Setter voor het genre van de artiest
     public void setGenre(String genre) {
-        this.genre = genre;
+        this.genre = genre; // Wijs het opgegeven genre toe aan de artiest
     }
 
+    // Getter voor de artiestinformatie
     public String getArtistInfo() {
-        return artistInfo;
-    }
-    public void setArtistInfo(String artistInfo) {
-        this.artistInfo = artistInfo;
+        return artistInfo; // Geeft de artiestinformatie terug
     }
 
+    // Setter voor de artiestinformatie
+    public void setArtistInfo(String artistInfo) {
+        this.artistInfo = artistInfo; // Wijs de opgegeven artiestinformatie toe aan de artiest
+    }
+
+    // Override van de toString-methode om de naam van de artiest terug te geven als een String
     @Override
     public String toString() {
-        return getName();
+        return getName(); // Geeft de naam van de artiest terug
     }
 }
--- src/data/Performance.java
+++ src/data/Performance.java
@@ -2,88 +2,103 @@ package data;
 
 import java.io.Serializable;
 
+// Klasse om een optreden te vertegenwoordigen, implementeert Serializable zodat objecten kunnen worden omgezet in een stroom van bytes voor serialisatie
 public class Performance implements Serializable {
-    private Artist artist;
-    private Stage stage;
+    private Artist artist; // De artiest die optreedt
+    private Stage stage; // Het podium waarop het optreden plaatsvindt
 
-    private int beginHour;
-    private int beginMinute;
-    private int endHour;
-    private int endMinute;
+    private int beginHour; // Het uur waarop het optreden begint
+    private int beginMinute; // De minuut waarop het optreden begint
+    private int endHour; // Het uur waarop het optreden eindigt
+    private int endMinute; // De minuut waarop het optreden eindigt
 
-
-
+    // Constructor voor het maken van een optreden met opgegeven artiest, podium en begin- en eindtijd
     public Performance(Artist artist, Stage stage, int beginHour, int beginMinute, int endHour, int endMinute) {
-        this.artist = artist;
-        this.stage = stage;
-        this.beginHour = beginHour;
-        this.beginMinute = beginMinute;
-        this.endHour = endHour;
-        this.endMinute = endMinute;
+        this.artist = artist; // Wijs de opgegeven artiest toe aan het optreden
+        this.stage = stage; // Wijs het opgegeven podium toe aan het optreden
+        this.beginHour = beginHour; // Wijs het opgegeven beginuur toe aan het optreden
+        this.beginMinute = beginMinute; // Wijs de opgegeven beginminuut toe aan het optreden
+        this.endHour = endHour; // Wijs het opgegeven einduur toe aan het optreden
+        this.endMinute = endMinute; // Wijs de opgegeven eindminuut toe aan het optreden
     }
 
+    // Getter voor de artiest van het optreden
     public Artist getArtist() {
-        return artist;
+        return artist; // Geeft de artiest van het optreden terug
     }
 
+    // Setter voor de artiest van het optreden
     public void setArtist(Artist artist) {
-        this.artist = artist;
+        this.artist = artist; // Wijs de opgegeven artiest toe aan het optreden
     }
 
+    // Getter voor het podium van het optreden
     public Stage getStage() {
-        return stage;
+        return stage; // Geeft het podium van het optreden terug
     }
 
+    // Setter voor het podium van het optreden
     public void setStage(Stage stage) {
-        this.stage = stage;
+        this.stage = stage; // Wijs het opgegeven podium toe aan het optreden
     }
 
+    // Getter voor het beginuur van het optreden
     public int getBeginHour() {
-        return beginHour;
+        return beginHour; // Geeft het beginuur van het optreden terug
     }
 
+    // Setter voor het beginuur van het optreden
     public void setBeginHour(int beginHour) {
-        this.beginHour = beginHour;
+        this.beginHour = beginHour; // Wijs het opgegeven beginuur toe aan het optreden
     }
 
+    // Getter voor de beginminuut van het optreden
     public int getBeginMinute() {
-        return beginMinute;
+        return beginMinute; // Geeft de beginminuut van het optreden terug
     }
 
+    // Setter voor de beginminuut van het optreden
     public void setBeginMinute(int beginMinute) {
-        this.beginMinute = beginMinute;
+        this.beginMinute = beginMinute; // Wijs de opgegeven beginminuut toe aan het optreden
     }
 
+    // Getter voor het einduur van het optreden
     public int getEndHour() {
-        return endHour;
+        return endHour; // Geeft het einduur van het optreden terug
     }
 
+    // Setter voor het einduur van het optreden
     public void setEndHour(int endHour) {
-        this.endHour = endHour;
+        this.endHour = endHour; // Wijs het opgegeven einduur toe aan het optreden
     }
 
+    // Getter voor de eindminuut van het optreden
     public int getEndMinute() {
-        return endMinute;
+        return endMinute; // Geeft de eindminuut van het optreden terug
     }
 
+    // Setter voor de eindminuut van het optreden
     public void setEndMinute(int endMinute) {
-        this.endMinute = endMinute;
+        this.endMinute = endMinute; // Wijs de opgegeven eindminuut toe aan het optreden
     }
 
+    // Methode om de begintijd van het optreden als een String te krijgen
     public String getBeginTime() {
-        if (beginMinute < 10){
-            return (beginHour + ":0" + beginMinute);
+        if (beginMinute < 10) {
+            return (beginHour + ":0" + beginMinute); // Geeft de begintijd van het optreden terug als een String in het formaat "uu:mm"
         }
-        return (beginHour + ":" + beginMinute);
+        return (beginHour + ":" + beginMinute); // Geeft de begintijd van het optreden terug als een String in het formaat "uu:mm"
     }
 
+    // Methode om de eindtijd van het optreden als een String te krijgen
     public String getEndTime() {
-        if (endMinute < 10){
-            return (endHour + ":0" + endMinute);
+        if (endMinute < 10) {
+            return (endHour + ":0" + endMinute); // Geeft de eindtijd van het optreden terug als een String in het formaat "uu:mm"
         }
-        return (endHour + ":" + endMinute);
+        return (endHour + ":" + endMinute); // Geeft de eindtijd van het optreden terug als een String in het formaat "uu:mm"
     }
 
+    // Methode om de duur van het optreden in minuten te krijgen
     public int getDuration() {
         // Geeft de duur van het optreden terug in minuten
         int totalMinutes = 0;
@@ -114,9 +129,10 @@ public class Performance implements Serializable {
 
             }
         }
-        return totalMinutes;
+        return totalMinutes; // Geeft de totale duur van het optreden terug in minuten
     }
 
+    // Override van de toString-methode om een representatie van het optreden als een String terug te geven
     @Override
     public String toString() {
         return getArtist() + " at " + stage.getName();
