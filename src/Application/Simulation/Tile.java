712ea6918a1afbdbb13c41c1ee5e5e0b8b85cb5c
LeonBuijs <77409149+LeonBuijs@users.noreply.github.com>
2024-03-31 15:51:29 +0200

    Revert "Merge branch 'Pathfinding2' into Simulation"
    This reverts commit e70514b5339f51a3216107e1a018f74a32ee6fbb, reversing
    changes made to bc855598f67fd42f975997eea282c09424324b17.

---
+++ src/Application/Simulation/Tile.java
@@ -0,0 +1,79 @@
+package Application.Simulation;
+
+import java.util.ArrayList;
+
+public class Tile {
+    private int y;
+    private int x;
+    private int distanceValue;
+    private String ID;
+    private ArrayList<Tile> neighbours;
+    public Tile(int y, int x) {
+        this.y = y;
+        this.x = x;
+        this.ID = y+","+x;
+//        this.distanceValue = -1;
+        this.neighbours = new ArrayList<>();
+    }
+
+    public String getID() {
+        return ID;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int getPointX() {
+        return x*32;
+    }
+
+    public int getPointY() {
+        return y*32;
+    }
+
+    public void addNeighbour(Tile tile) {
+        neighbours.add(tile);
+    }
+    public void setDistanceValue(int distanceValue) {
+        this.distanceValue = distanceValue;
+    }
+
+    public int getDistanceValue() {
+        return distanceValue;
+    }
+
+    public boolean isNeighbour(Tile tile) {
+        return neighbours.contains(tile);
+    }
+
+    public ArrayList<Tile> getNeighbours() {
+        System.out.println("Tile: "+ this + " has neighbours " + neighbours);
+        return neighbours;
+    }
+
+    @Override
+    public String toString() {
+        return "Tile{" +
+                "y=" + y +
+                ", x=" + x +
+                ", ID='" + ID + '\'' +
+                '}';
+    }
+
+    public void printNeighbours() {
+        System.out.println(neighbours);
+    }
+}
--- src/Application/Simulation/Visitor.java
+++ src/Application/Simulation/Visitor.java
@@ -8,61 +8,87 @@ import java.awt.geom.Point2D;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Visitor {
 
+    private PathFinder pathFinder;
     private Point2D position;
     private Point2D targetPosition;
     private double angle;
     private double speed;
+    private double currentDistance = Integer.MAX_VALUE;
     private BufferedImage image;
+    private Tile currentTile;
 
-    public Visitor(Point2D position, double speed) {
+    public Visitor(Point2D position, PathFinder pathFinder, double speed) {
         try {
             this.image = ImageIO.read(this.getClass().getResourceAsStream("/Visitors/MV_Graveyard_Zombies_Skeleton.png"));
             this.image = image.getSubimage(0, 0, image.getWidth() / 11, image.getHeight() / 6);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-
+        this.pathFinder = pathFinder;
         this.position = position;
+        this.currentTile = pathFinder.getTileFromPosition(new Point2D.Double(126, 64));
         this.targetPosition = position;
-        this.angle = 0;
         this.speed = speed;
+        this.angle = 0;
     }
 
     public void update(ArrayList<Visitor> visitors) {
-        if (this.position == this.targetPosition) {
-            return;
-        }
-        double newAngle = Math.atan2(targetPosition.getY() - position.getY(), targetPosition.getX() - position.getX());
+        for (Tile tile : currentTile.getNeighbours()) {
 
-        double difference = angle - newAngle;
-        
-        while (difference > Math.PI) {
-            difference -= 2 * Math.PI;
-        }
-        while (difference < -Math.PI) {
-            difference += 2 * Math.PI;
+            if (this.pathFinder.path.get(tile) == null)
+                continue;
+
+            int newDistance = this.pathFinder.path.get(tile);
+
+            if (newDistance < currentDistance) {
+                currentDistance = newDistance;
+
+                double x = tile.getPointX();
+                double y = tile.getPointY();
+
+                this.targetPosition = new Point2D.Double(x, y);
+                this.currentTile = pathFinder.getTileFromPosition(new Point2D.Double(x/32, y/32));
+
+                //break when a closer tile is found
+                break;
+            }
         }
 
-        if (difference < -0.1) {
+        double newAngle = Math.atan2(this.targetPosition.getY() - this.position.getY(), this.targetPosition.getX() - this.position.getX());
+
+        double angleDifference = angle - newAngle;
+
+        while (angleDifference > Math.PI) {
+            angleDifference -= 2 * Math.PI;
+        }
+
+        while (angleDifference < -Math.PI) {
+            angleDifference += 2 * Math.PI;
+        }
+
+        if (angleDifference < -0.1) {
             angle += 0.1;
-        } else if (difference > 0.1) {
+        } else if (angleDifference > 0.1) {
             angle -= 0.1;
-        } else
+        } else {
             angle = newAngle;
+        }
 
         Point2D newPosition = new Point2D.Double(
-                position.getX() + speed * Math.cos(angle),
-                position.getY() + speed * Math.sin(angle)
+                position.getX() + speed * Math.cos(angle) * targetPosition.getX(),
+                position.getY() + speed * Math.sin(angle) * targetPosition.getY()
         );
 
         boolean collision = false;
 
         for (Visitor visitor : visitors) {
             if (visitor != this) {
-                if (visitor.position.distance(newPosition) <= 64) {
+                //32 so the heads can collide (giving the 2.5D effect we want)
+                if (visitor.position.distance(newPosition) <= 32) {
                     collision = true;
                 }
             }
@@ -78,12 +104,14 @@ public class Visitor {
     public void draw(Graphics2D graphics2D) {
         AffineTransform transform = new AffineTransform();
 
-        transform.translate(position.getX() - image.getWidth() / 2, position.getY() - image.getHeight() / 2);
-        transform.rotate(this.angle, image.getWidth() / 2, image.getHeight() / 2);
+        transform.translate(position.getX() - image.getWidth() /2 , position.getY() - image.getHeight() /1.5);
 
         graphics2D.setColor(Color.red);
         graphics2D.draw(new Ellipse2D.Double(this.targetPosition.getX(), this.targetPosition.getY(), 10, 10));
         graphics2D.drawImage(this.image, transform, null);
+        graphics2D.setColor(Color.green);
+        graphics2D.fill(new Ellipse2D.Double(this.position.getX(), this.position.getY(), 10,10));
+        graphics2D.setColor(Color.black);
     }
 
     public void setTargetPosition(Point2D targetPosition) {
