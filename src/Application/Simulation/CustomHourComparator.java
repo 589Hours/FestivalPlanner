23f81d7081fab05d150756201b152f4d2a32cf7a
Kwint <kwintjanleenders@gmail.com>
2024-04-01 15:18:36 +0200

    CustomHourComparator werkt nu

--- src/Application/Simulation/CustomHourComparator.java
+++ src/Application/Simulation/CustomHourComparator.java
@@ -1,19 +1,17 @@
-//package Application.Simulation;
-//
-//import data.Performance;
-//
-//import java.util.Comparator;
-//
-//public class CustomHourComparator implements Comparator<Performance> {
-//
-//    @Override
-//    public int compare(Performance p1, Performance p2) {
-////        int beginHour1 = p1.getBeginHour();
-////        int byHour = beginHour1.compareTo(p2.getBeginHour());
-////        if(byHour == 0) {
-////
-////            return p1.compareTo(p2.getBeginMinute());
-////        }
-////        return byHour;
-////    }
-//}
+package Application.Simulation;
+
+import data.Performance;
+
+import java.util.Comparator;
+
+public class CustomHourComparator implements Comparator<Performance> {
+
+    @Override
+    public int compare(Performance p1, Performance p2) {
+        int byHour = Integer.compare(p1.getBeginHour(), p2.getBeginHour());
+        if(byHour == 0) {
+            return Integer.compare(p1.getBeginMinute(), p2.getBeginMinute());
+        }
+        return byHour;
+    }
+}
