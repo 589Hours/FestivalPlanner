7daaa2b837c63fd293e58eebea42916fcd46b813
LeonBuijs <77409149+LeonBuijs@users.noreply.github.com>
2024-04-02 14:47:51 +0200

    X positie gefixed met klikken

--- src/Application/Simulation/Simulation.java
+++ src/Application/Simulation/Simulation.java
@@ -47,7 +47,8 @@ public class Simulation extends Application {
         canvas.setWidth(1024);
         canvas.setHeight(1024);
 
-        canvas.setOnMousePressed(event -> checkClicked(new Point2D.Double(event.getX()*(1/camera.getTransform().getScaleX()), event.getY()*(1/camera.getTransform().getScaleX()))));
+        canvas.setOnMousePressed(event -> checkClicked(new Point2D.Double((event.getX() - camera.getTransform().getTranslateX())*(1/camera.getTransform().getScaleX()),
+                (event.getY() - camera.getTransform().getTranslateX())*(1/camera.getTransform().getScaleX()))));
         canvas.setOnScroll(event -> camera.mouseScroll(event));
 //        canvas.setOnMouseMoved(event -> {
 //            try {
